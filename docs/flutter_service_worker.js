91a676e6405ef00805479361e71b7043c20dbc52
gl3ncc <23000237@myrp.edu.sg>
2025-07-25 23:17:59 +0800

    Deploy latest build

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "firebase-config.js": "7572489006c0d32c69970b9f77737bdf",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "a76f4353919123b5057f8e2e51f59357",
+"flutter_bootstrap.js": "350e45f00ce60a1c5b9325543896ada3",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
