a514316b2d85c4b21515eab273ba545922f59d03
gl3ncc <23000237@myrp.edu.sg>
2025-07-28 12:09:30 +0800

    Deploy latest web build to docs

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -26,14 +26,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "firebase-config.js": "7572489006c0d32c69970b9f77737bdf",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "f1d2b66591b182a78c7ed47588c299ce",
+"flutter_bootstrap.js": "48363afe3ca5625e461f2a4dc74f5058",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "e6ea516524f9c0fe7cb8d21049f50eef",
 "/": "e6ea516524f9c0fe7cb8d21049f50eef",
-"main.dart.js": "f212ddc4c3a1045c300d662924becfbb",
+"main.dart.js": "a043c920baaf403c5256ce7dc19f9b29",
 "manifest.json": "d41d8cd98f00b204e9800998ecf8427e",
 "version.json": "341861d79787b393cc2db6de14c83c7c"};
 // The application shell files that are downloaded before a service worker can
